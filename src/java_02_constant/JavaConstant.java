2beffc9e98f98053b19415fab43c65554524495e
yunqiang <huangyunqiang@wxblockchain.com>
2021-05-06 17:38:49 +0800

    java常量

---
+++ src/java_02_constant/JavaConstant.java
@@ -0,0 +1,18 @@
+package java_02_constant;
+
+public class JavaConstant {
+    public static void main(String[] args) {
+        // 字符串常量
+        System.out.println("字符串常量");
+        // 整数常量
+        System.out.println(100);
+        // 小数常量
+        System.out.println(13.14);
+        // 字符常量
+        System.out.println('a');
+        // 布尔常量
+        System.out.println(false);
+        // 空常量,不能直接输出
+        //System.out.println(null);
+    }
+}
