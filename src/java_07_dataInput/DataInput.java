f6f80bfd5146d7ee2bb08dc65cb784c84b2596b7
yunqiang <huangyunqiang@wxblockchain.com>
2021-05-07 16:42:19 +0800

    java数据键盘输入

--- src/java_07_dataInput/DataInput.java
+++ src/java_07_dataInput/DataInput.java
@@ -1,2 +1,19 @@
-package java_07_dataInput;public class DataInput {
+package java_07_dataInput;
+
+import java.util.Scanner;  // 导包
+
+public class DataInput {
+
+    static void datainput(){
+        Scanner sc = new Scanner(System.in); // 创建对象
+        int i = sc.nextInt(); // 接收键盘输入
+        String s = sc.next(); // 接收键盘输入
+        System.out.println("键盘录入int为："+i);
+        System.out.println("键盘录入为："+s);
+
+    }
+
+    public static void main(String[] args) {
+        datainput();
+    }
 }
--- src/java_07_dataInput/Demo_01.java
+++ src/java_07_dataInput/Demo_01.java
@@ -1,2 +1,26 @@
-package java_07_dataInput;public class Demo_01 {
+package java_07_dataInput;
+
+import java.util.Scanner;
+
+public class Demo_01 {
+
+    static void threePerson() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入第一个人的身高：");
+        int height1 = sc.nextInt();
+        System.out.println("请输入第二个人的身高：");
+        int height2 = sc.nextInt();
+        System.out.println("请输入第三个人的身高：");
+        int height3 = sc.nextInt();
+
+        int tempheight = height1 > height2 ? height1 : height2;
+        int maxHeight = tempheight > height3 ? tempheight : height3;
+
+        System.out.println("这三个人中身高最高的是：" + maxHeight);
+    }
+
+
+    public static void main(String[] args) {
+        threePerson();
+    }
 }
