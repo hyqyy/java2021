c081fcc122f7953365234c64ba6e2a0a7eeaf884
yunqiang <huangyunqiang@wxblockchain.com>
2021-05-10 14:26:46 +0800

    java类定义使用

---
+++ src/java_13_class/Student.java
@@ -0,0 +1,26 @@
+package java_13_class;
+
+public class Student {
+
+    // 成员变量
+    String name;
+    int age;
+
+    // 成员方法
+    public void study() {
+        System.out.println("好好学习");
+    }
+
+    // 成员方法,带返回值，参数
+    public String doHomework(String a) {
+
+        // 局部变量
+        int x;
+        int y = 11;
+        //        System.out.println(x);  //局部变量不赋值，不会自动初始化默认值，故：不赋值不可以使用
+        System.out.println("y=" + y);
+
+
+        return a + "多做练习";
+    }
+}
---
+++ src/java_13_class/StudentDemo.java
@@ -0,0 +1,16 @@
+package java_13_class;
+
+public class StudentDemo {
+
+    public static void main(String[] args) {
+        Student st = new Student();
+        System.out.println(st.name +","+st.age);
+        st.name = "john";
+        st.age = 10;
+        System.out.println(st.name +","+st.age);
+        st.study();
+        String result = st.doHomework("放学要");
+        System.out.println(result);
+
+    }
+}
