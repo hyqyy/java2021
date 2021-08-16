5fd687b317e99234606c34e38d197cc5bc964a78
yunqiang <huangyunqiang@wxblockchain.com>
2021-08-16 16:37:04 +0800

    增加自定义异常测试

--- src/java_25_exception/TestScoreException.java
+++ src/java_25_exception/TestScoreException.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class TestScoreException {
 
     public static void method() throws ScoreException {
-
+        System.out.println("请录入分数：");
         Scanner sc = new Scanner(System.in);
         int i = sc.nextInt();
         if (i > 100 || i < 0) {
             Throwable th = new Throwable("录入错误");
-            throw new ScoreException("分数应该介于0-100之间", th); // 抛出自定义异常
+            throw new ScoreException("分数应该介于0-100之间", th); // 抛出自定义异常对象
         } else {
             System.out.println("分数正常");
         }
@@ -23,7 +23,8 @@ public class TestScoreException {
         try {
             method();
         } catch (Exception e){
-            System.out.println(e);
+            System.out.println("错误原因" +  e.getCause());
+            System.out.println("错误信息：" + e.getMessage());
         }
 
     }
